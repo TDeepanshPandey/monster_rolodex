62474bbf8829db35119624bd1a44a3c61c515335
Deepansh Pandey <easypandey.dp@gmail.com>
2022-09-30 23:43:14 +0200

    Trying state for first time

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      name:'Yihua'
+    }
+  }
   render(){
   return (
     <div className="App">
@@ -11,6 +17,7 @@ class App extends Component{
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <span>Hi {this.state.name}</span>
         <a
           className="App-link"
           href="https://reactjs.org"
