27912f04c8b29a6e151f97a821f54e067998698b
Deepansh Pandey <easypandey.dp@gmail.com>
2025-01-29 23:31:57 +0100

    Adding new files

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import './App.css'
+import { Component } from 'react'
+
+class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      monsters: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then((users) => this.setState(() => {
+        return{monsters: users}
+      }))
+  }
+
+  render() {
+    return (
+        <div className='App'>
+          <input className='search-box' type='search' placeholder='Search Monster' onChange={
+            (event) => {
+              const searchString = event.target.value.toLocaleLowerCase()
+              const filteredMonsters = this.state.monsters.filter((monster) => {
+                return monster.name.toLocaleLowerCase().includes(searchString)
+              })
+              this.setState(() => {
+                return { monsters: filteredMonsters }
+              })
+            }}/>
+          {
+            this.state.monsters.map((monster) => {
+              return (
+                <div key={monster.id}>
+                  <h1>{monster.name}</h1>
+                </div>)
+            })
+          }
+        </div>
+    )
+  }
+}
+
+export default App
